e7cfdd1b7977f0cd93c3a8411a0fd0fd89f30ba8
WhyYouAre <whyouare556@protonmail.com>
2021-08-11 14:23:39 +0800

    fix: configKey enum  to constants

--- src/config-biz/config-biz.service.ts
+++ src/config-biz/config-biz.service.ts
@@ -3,17 +3,20 @@ import { config, WATCHER } from './configuration.biz';
 import { Cache } from 'cache-manager';
 import * as objectPath from 'object-path';
 
-enum Key {
-  HEX_GRID_FORBIDDEN_ZONE_RADIUS = 'hex_grid.forbidden_zone.radius',
-  HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED = 'hex_grid.feature_flags.new_invitation_slot_created_on_hex_grid_occupied',
-}
-
+const HEX_GRID_FORBIDDEN_ZONE_RADIUS = 'hex_grid.forbidden_zone.radius';
+const HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED =
+  'hex_grid.feature_flags.new_invitation_slot_created_on_hex_grid_occupied';
+const AVAILABLE_KEYS = [
+  HEX_GRID_FORBIDDEN_ZONE_RADIUS,
+  HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
+];
 @Injectable()
 export class ConfigBizService {
   private logger = new Logger(ConfigBizService.name);
   constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
 
   onModuleInit() {
+    this.logger.debug('onModuleInit');
     this.refreshCache();
     WATCHER.on('change', (path: string) => {
       this.logger.log(`File ${path} changed. Refresh cache...`);
@@ -24,15 +27,16 @@ export class ConfigBizService {
   private refreshCache() {
     // 统一刷新就不需要每个值都读取 YAML
     const myConfig = config();
-    for (const value in Key) {
-      this.reloadValue(value, myConfig);
-    }
+    AVAILABLE_KEYS.forEach((key) => {
+      this.reloadValue(key, myConfig);
+    });
   }
 
   private async loadValue<T = any>(
     key: string,
     myConfig = config(),
   ): Promise<T> {
+    console.log(myConfig);
     let cachedValue = await this.cache.get<T>(key);
     if (cachedValue !== null) {
       this.logger.debug(`get cached value: ${key} = ${cachedValue}`);
@@ -60,12 +64,12 @@ export class ConfigBizService {
   }
 
   async getHexGridForbiddenZoneRadius(): Promise<number> {
-    return this.loadValue<number>(Key.HEX_GRID_FORBIDDEN_ZONE_RADIUS);
+    return this.loadValue<number>(HEX_GRID_FORBIDDEN_ZONE_RADIUS);
   }
 
   async isNewInvitationSlotCreatedOnHexGridOccupiedEnabled(): Promise<boolean> {
     return this.loadValue<boolean>(
-      Key.HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
+      HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
     );
   }
 }
