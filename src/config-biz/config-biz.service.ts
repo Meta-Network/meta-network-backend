c812e14a7c1c58ccc16da7d5e71850a955726dd6
WhyYouAre <whyouare556@protonmail.com>
2021-08-11 13:59:40 +0800

    refactor: load yaml once when refresh cache

--- src/config-biz/config-biz.service.ts
+++ src/config-biz/config-biz.service.ts
@@ -3,9 +3,10 @@ import { config, WATCHER } from './configuration.biz';
 import { Cache } from 'cache-manager';
 import * as objectPath from 'object-path';
 
-const KEY_HEX_GRID_FORBIDDEN_ZONE_RADIUS = 'hex_grid.forbidden_zone.radius';
-const KEY_HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED =
-  'hex_grid.feature_flags.new_invitation_slot_created_on_hex_grid_occupied';
+enum Key {
+  HEX_GRID_FORBIDDEN_ZONE_RADIUS = 'hex_grid.forbidden_zone.radius',
+  HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED = 'hex_grid.feature_flags.new_invitation_slot_created_on_hex_grid_occupied',
+}
 
 @Injectable()
 export class ConfigBizService {
@@ -20,44 +21,51 @@ export class ConfigBizService {
     });
   }
 
-  refreshCache() {
-    this.reloadValue(KEY_HEX_GRID_FORBIDDEN_ZONE_RADIUS);
-    this.reloadValue(
-      KEY_HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
-    );
+  private refreshCache() {
+    // 统一刷新就不需要每个值都读取 YAML
+    const myConfig = config();
+    for (const value in Key) {
+      this.reloadValue(value, myConfig);
+    }
   }
 
-  async loadValue<T = any>(key: string): Promise<T> {
+  private async loadValue<T = any>(
+    key: string,
+    myConfig = config(),
+  ): Promise<T> {
     let cachedValue = await this.cache.get<T>(key);
     if (cachedValue !== null) {
       this.logger.debug(`get cached value: ${key} = ${cachedValue}`);
       return cachedValue;
     }
-    const myConfig = config();
+
     cachedValue = objectPath.get(myConfig, key);
     this.logger.debug(`set cachedValue: ${key} = ${cachedValue}`);
     await this.cache.set(key, cachedValue);
     return await this.cache.get<T>(key);
   }
 
-  async reloadValue<T = any>(key: string): Promise<T> {
+  private async reloadValue<T = any>(
+    key: string,
+    myConfig = config(),
+  ): Promise<T> {
     try {
       this.logger.log(`reload cache: ${key}`);
 
       await this.cache.del(key);
-      return await this.loadValue(key);
+      return await this.loadValue(key, myConfig);
     } catch (err) {
       this.logger.error(`reload cache: ${key} error`, err);
     }
   }
 
   async getHexGridForbiddenZoneRadius(): Promise<number> {
-    return this.loadValue<number>(KEY_HEX_GRID_FORBIDDEN_ZONE_RADIUS);
+    return this.loadValue<number>(Key.HEX_GRID_FORBIDDEN_ZONE_RADIUS);
   }
 
   async isNewInvitationSlotCreatedOnHexGridOccupiedEnabled(): Promise<boolean> {
     return this.loadValue<boolean>(
-      KEY_HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
+      Key.HEX_GRID_FEATURE_FLAGS_NEW_INTIVATION_SLOT_CREATED_ON_HEX_GRID_OCCUPIED,
     );
   }
 }
