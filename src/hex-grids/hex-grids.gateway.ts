ca08666eb42055181492b03d1cb6422da96daf89
WhyYouAre <whyouare556@protonmail.com>
2021-08-19 17:33:37 +0800

    style: remove unused imports & comments

--- src/hex-grids/hex-grids.gateway.ts
+++ src/hex-grids/hex-grids.gateway.ts
@@ -2,22 +2,14 @@ import { Logger } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import {
   WebSocketGateway,
-  SubscribeMessage,
-  MessageBody,
-  WsResponse,
   OnGatewayInit,
   OnGatewayDisconnect,
   OnGatewayConnection,
 } from '@nestjs/websockets';
-import { AnyARecord } from 'node:dns';
-import { Observable, Subject, map, filter } from 'rxjs';
-import loadConfig from '../config/configuration';
 import { HexGrid } from '../entities/hex-grid.entity';
-import { FindByFilterDto } from './dto/find-by-filter.dto';
 import { HexGridsEvent } from './hex-grids.constant';
 import { Server } from 'socket.io';
-const { app } = loadConfig();
-// @WebSocketGateway(app.port, { namespace: 'hex-grids' })
+
 @WebSocketGateway({
   namespace: 'hex-grids',
   origin: ['*'],
@@ -27,16 +19,14 @@ export class HexGridsGateway
   implements OnGatewayInit, OnGatewayDisconnect, OnGatewayConnection
 {
   private readonly logger = new Logger(HexGridsGateway.name);
-  // constructor(private readonly hexGridsService: HexGridsService) {}
-  // private readonly occupyHexGridSubject = new Subject<HexGrid>();
+
   private server: Server;
-  afterInit(server: any) {
-    // console.log(server);
+  afterInit(server: Server) {
     this.logger.log(server);
     this.server = server;
   }
 
-  handleDisconnect(client: AnyARecord) {
+  handleDisconnect(client: any) {
     this.logger.debug(client);
   }
   handleConnection(client: any, ...args: any[]) {
@@ -44,29 +34,6 @@ export class HexGridsGateway
   }
   @OnEvent(HexGridsEvent.OCCUPIED)
   async handleAppHexGridOccupiedEvent(payload: HexGrid) {
-    // this.occupyHexGridSubject.next(payload);
     this.server.emit(HexGridsEvent.OCCUPIED, payload);
   }
-
-  // @SubscribeMessage(HexGridsEvent.OCCUPIED)
-  // handleWsHexGridOccupiedEvent(
-  //   @MessageBody() data: FindByFilterDto,
-  // ): Observable<WsResponse<HexGrid>> {
-  //   //TODO 可以考虑在data里添加filter
-  //   console.log(data);
-  //   return this.occupyHexGridSubject
-  //     .pipe(
-  //       filter((item) => {
-  //         return !(
-  //           (data.xMin && item.x < data.xMin) ||
-  //           (data.xMax && item.x > data.xMax) ||
-  //           (data.yMin && item.y < data.yMin) ||
-  //           (data.yMax && item.y > data.yMax) ||
-  //           (data.zMin && item.z < data.zMin) ||
-  //           (data.zMax && item.z > data.zMax)
-  //         );
-  //       }),
-  //     )
-  //     .pipe(map((item) => ({ event: HexGridsEvent.OCCUPIED, data: item })));
-  // }
 }
