f932474520fbf7a13a9ccc420f31160e722c3f95
神樹桜乃 <kodama@sakuno.moe>
2021-12-06 22:37:43 +0800

    chore: add entities injection

--- src/hex-grids/hex-grids.module.ts
+++ src/hex-grids/hex-grids.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
-import { HexGridsService } from './hex-grids.service';
-import { HexGridsController } from './hex-grids.controller';
-import { HexGrid } from '../entities/hex-grid.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HexGridBatchTxEntity } from 'src/entities/hex-grid-batch-tx.entity';
+import { HexGridPendingEntity } from 'src/entities/hex-grid-pending.entity';
+import { HexGridTransactionReferenceEntity } from 'src/entities/hex-grid-tx-ref.entity';
+
 import { ConfigBizModule } from '../config-biz/config-biz.module';
+import { HexGrid } from '../entities/hex-grid.entity';
+import { HexGridsController } from './hex-grids.controller';
 import { HexGridsGateway } from './hex-grids.gateway';
+import { HexGridsService } from './hex-grids.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HexGrid]), ConfigBizModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      HexGrid,
+      HexGridBatchTxEntity,
+      HexGridPendingEntity,
+      HexGridTransactionReferenceEntity,
+    ]),
+    ConfigBizModule,
+  ],
   controllers: [HexGridsController],
   providers: [HexGridsService, HexGridsGateway],
   exports: [HexGridsService, HexGridsGateway],
