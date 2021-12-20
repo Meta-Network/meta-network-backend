45ec3e8ff0b1f9d332acff1f1ae02b640f5695eb
神樹桜乃 <kodama@sakuno.moe>
2021-12-20 18:13:39 +0800

    feat: add scheduler logging

--- src/scheduler/hex-grid/hex-grid.scheduler.ts
+++ src/scheduler/hex-grid/hex-grid.scheduler.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { Connection } from 'typeorm';
 
-import { TypeOrmHexGridStorage } from './hex-grid-storage-typeorm';
 import { HexGridStorageService } from './hex-grid-storage.service';
+import { TypeOrmHexGridStorage } from './hex-grid-storage-typeorm';
 
 @Injectable()
 export class HexGridScheduler {
+  private readonly logger = new Logger(HexGridScheduler.name);
+
   constructor(
     private connection: Connection,
     private storageService: HexGridStorageService,
@@ -14,6 +16,8 @@ export class HexGridScheduler {
 
   @Cron('0 */5 * * * *')
   async handleBatchUpload() {
+    this.logger.log('triggered');
+
     await this.connection.transaction(async (manager) => {
       await this.storageService.uploadPendings(
         new TypeOrmHexGridStorage(manager),
